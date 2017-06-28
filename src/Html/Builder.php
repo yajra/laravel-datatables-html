3251bc5b7fc2cce8b912c848c1903aed73cd3649
Arjay Angeles <aqangeles@gmail.com>
2017-06-28 12:57:28 +0800

    Use config repository instead of helper.

--- src/Html/Builder.php
+++ src/Html/Builder.php
@@ -505,7 +505,7 @@ class Builder
      */
     public function addIndex(array $attributes = [])
     {
-        $indexColumn = config('datatables.index_column', 'DT_Row_Index');
+        $indexColumn = $this->config->get('datatables.index_column', 'DT_Row_Index');
         $attributes  = array_merge([
             'defaultContent' => '',
             'data'           => $indexColumn,
