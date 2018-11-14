3e7a7519405493b6eaa814c4773ca3df89a365e3
Arjay Q. Angeles <aqangeles@gmail.com>
2018-11-14 12:44:06 +0800

    Refactor DataTables - Data ajax option builder.

--- src/Html/Builder.php
+++ src/Html/Builder.php
@@ -16,6 +16,7 @@ class Builder
 {
     use Macroable;
     use HasDataTablesFeaturesOptions;
+    use HasDataTablesDataOptions;
 
     /**
      * @var Collection
@@ -36,11 +37,6 @@ class Builder
      * @var HtmlBuilder
      */
     public $html;
-
-    /**
-     * @var string|array
-     */
-    protected $ajax = '';
 
     /**
      * @var array
@@ -505,52 +501,6 @@ class Builder
     }
 
     /**
-     * Setup ajax parameter for datatables pipeline plugin.
-     *
-     * @param  string $url
-     * @param  string $pages
-     * @return $this
-     */
-    public function pipeline($url, $pages)
-    {
-        $this->ajax = "$.fn.dataTable.pipeline({ url: '{$url}', pages: {$pages} })";
-
-        return $this;
-    }
-
-    /**
-     * Setup "ajax" parameter with POST method.
-     *
-     * @param  string|array $attributes
-     * @return $this
-     */
-    public function postAjax($attributes = '')
-    {
-        if (! is_array($attributes)) {
-            $attributes = ['url' => (string) $attributes];
-        }
-
-        unset($attributes['method']);
-        Arr::set($attributes, 'type', 'POST');
-        Arr::set($attributes, 'headers.X-HTTP-Method-Override', 'GET');
-
-        return $this->ajax($attributes);
-    }
-
-    /**
-     * Setup ajax parameter.
-     *
-     * @param  string|array $attributes
-     * @return $this
-     */
-    public function ajax($attributes = '')
-    {
-        $this->ajax = $attributes;
-
-        return $this;
-    }
-
-    /**
      * Generate DataTable's table html.
      *
      * @param array $attributes
@@ -675,51 +625,6 @@ class Builder
     }
 
     /**
-     * Minify ajax url generated when using get request
-     * by deleting unnecessary url params.
-     *
-     * @param string $url
-     * @param string $script
-     * @param array $data
-     * @param array $ajaxParameters
-     * @return $this
-     */
-    public function minifiedAjax($url = '', $script = null, $data = [], $ajaxParameters = [])
-    {
-        $this->ajax = [];
-        $appendData = $this->makeDataScript($data);
-
-        $this->ajax['url']  = $url;
-        $this->ajax['type'] = 'GET';
-        if (isset($this->attributes['serverSide']) ? $this->attributes['serverSide'] : true) {
-            $this->ajax['data'] = 'function(data) {
-            for (var i = 0, len = data.columns.length; i < len; i++) {
-                if (!data.columns[i].search.value) delete data.columns[i].search;
-                if (data.columns[i].searchable === true) delete data.columns[i].searchable;
-                if (data.columns[i].orderable === true) delete data.columns[i].orderable;
-                if (data.columns[i].data === data.columns[i].name) delete data.columns[i].name;
-            }
-            delete data.search.regex;';
-        } else {
-            $this->ajax['data'] = 'function(data){';
-        }
-
-        if ($appendData) {
-            $this->ajax['data'] .= $appendData;
-        }
-
-        if ($script) {
-            $this->ajax['data'] .= $script;
-        }
-
-        $this->ajax['data'] .= '}';
-
-        $this->ajax = array_merge($this->ajax, $ajaxParameters);
-
-        return $this;
-    }
-
-    /**
      * Make a data script to be appended on ajax request of dataTables.
      *
      * @param array $data
@@ -804,20 +709,6 @@ class Builder
         }
 
         return $editor;
-    }
-
-    /**
-     * Get ajax url.
-     *
-     * @return array|mixed|string
-     */
-    public function getAjaxUrl()
-    {
-        if (is_array($this->ajax)) {
-            return $this->ajax['url'] ?: url()->current();
-        }
-
-        return $this->ajax ?: url()->current();
     }
 
     /**
---
+++ src/Html/HasDataTablesDataOptions.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Yajra\DataTables\Html;
+
+/**
+ * DataTables - Data option builder.
+ *
+ * @see https://datatables.net/reference/option/
+ */
+trait HasDataTablesDataOptions
+{
+    /**
+     * @var string|array
+     */
+    protected $ajax = '';
+
+    /**
+     * Setup ajax parameter.
+     *
+     * @param  string|array $attributes
+     * @return $this
+     */
+    public function ajax($attributes = '')
+    {
+        $this->ajax = $attributes;
+
+        return $this;
+    }
+
+    /**
+     * Setup "ajax" parameter with POST method.
+     *
+     * @param  string|array $attributes
+     * @return $this
+     */
+    public function postAjax($attributes = '')
+    {
+        if (! is_array($attributes)) {
+            $attributes = ['url' => (string) $attributes];
+        }
+
+        unset($attributes['method']);
+        array_set($attributes, 'type', 'POST');
+        array_set($attributes, 'headers.X-HTTP-Method-Override', 'GET');
+
+        return $this->ajax($attributes);
+    }
+
+    /**
+     * Setup ajax parameter for datatables pipeline plugin.
+     *
+     * @param  string $url
+     * @param  string $pages
+     * @return $this
+     */
+    public function pipeline($url, $pages)
+    {
+        $this->ajax = "$.fn.dataTable.pipeline({ url: '{$url}', pages: {$pages} })";
+
+        return $this;
+    }
+
+    /**
+     * Minify ajax url generated when using get request
+     * by deleting unnecessary url params.
+     *
+     * @param string $url
+     * @param string $script
+     * @param array $data
+     * @param array $ajaxParameters
+     * @return $this
+     */
+    public function minifiedAjax($url = '', $script = null, $data = [], $ajaxParameters = [])
+    {
+        $this->ajax = [];
+        $appendData = $this->makeDataScript($data);
+
+        $this->ajax['url']  = $url;
+        $this->ajax['type'] = 'GET';
+        if (isset($this->attributes['serverSide']) ? $this->attributes['serverSide'] : true) {
+            $this->ajax['data'] = 'function(data) {
+            for (var i = 0, len = data.columns.length; i < len; i++) {
+                if (!data.columns[i].search.value) delete data.columns[i].search;
+                if (data.columns[i].searchable === true) delete data.columns[i].searchable;
+                if (data.columns[i].orderable === true) delete data.columns[i].orderable;
+                if (data.columns[i].data === data.columns[i].name) delete data.columns[i].name;
+            }
+            delete data.search.regex;';
+        } else {
+            $this->ajax['data'] = 'function(data){';
+        }
+
+        if ($appendData) {
+            $this->ajax['data'] .= $appendData;
+        }
+
+        if ($script) {
+            $this->ajax['data'] .= $script;
+        }
+
+        $this->ajax['data'] .= '}';
+
+        $this->ajax = array_merge($this->ajax, $ajaxParameters);
+
+        return $this;
+    }
+
+    /**
+     * Get ajax url.
+     *
+     * @return array|mixed|string
+     */
+    public function getAjaxUrl()
+    {
+        if (is_array($this->ajax)) {
+            return $this->ajax['url'] ?: url()->current();
+        }
+
+        return $this->ajax ?: url()->current();
+    }
+}
