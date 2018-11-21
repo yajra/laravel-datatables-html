44d1875fba565e6006d6c28a84152c8f3137db25
Arjay Q. Angeles <aqangeles@gmail.com>
2018-11-21 12:17:34 +0800

    Add full support for RowReorder plugin builder. As per https://datatables.net/reference/option/#scroller.

--- src/Html/Options/Plugins/RowReorder.php
+++ src/Html/Options/Plugins/RowReorder.php
@@ -7,6 +7,7 @@ namespace Yajra\DataTables\Html\Options\Plugins;
  *
  * @see https://datatables.net/extensions/rowreorder
  * @see https://datatables.net/reference/option/rowReorder
+ * @see https://datatables.net/reference/option/#rowReorder
  */
 trait RowReorder
 {
@@ -20,6 +21,104 @@ trait RowReorder
     public function rowReorder($value = true)
     {
         $this->attributes['rowReorder'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set rowReorder dataSrc option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/rowReorder.dataSrc
+     */
+    public function rowReorderDataSrc($value = 0)
+    {
+        $this->attributes['rowReorder']['dataSrc'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set rowReorder editor option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/rowReorder.editor
+     */
+    public function rowReorderEditor($value = null)
+    {
+        $this->attributes['rowReorder']['editor'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set rowReorder enable option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/rowReorder.enable
+     */
+    public function rowReorderEnable(bool $value = true)
+    {
+        $this->attributes['rowReorder']['enable'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set rowReorder formOptions option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/rowReorder.formOptions
+     */
+    public function rowReorderFormOptions($value)
+    {
+        $this->attributes['rowReorder']['formOptions'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set rowReorder selector option value.
+     *
+     * @param string $value
+     * @return $this
+     * @see https://datatables.net/reference/option/rowReorder.selector
+     */
+    public function rowReorderSelector($value = 'td:first-child')
+    {
+        $this->attributes['rowReorder']['selector'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set rowReorder snapX option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/rowReorder.snapX
+     */
+    public function rowReorderSnapX($value = true)
+    {
+        $this->attributes['rowReorder']['snapX'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set rowReorder update option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/rowReorder.update
+     */
+    public function rowReorderUpdate(bool $value = true)
+    {
+        $this->attributes['rowReorder']['update'] = $value;
 
         return $this;
     }
