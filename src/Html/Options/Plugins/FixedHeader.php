322ab56d717fa3c868a1f66d21bd5a5ae80186f6
Arjay Q. Angeles <aqangeles@gmail.com>
2018-11-21 15:12:16 +0800

    Add full support for FixedHeader plugin builder. As per https://datatables.net/reference/option/#fixedHeader.

--- src/Html/Options/Plugins/FixedHeader.php
+++ src/Html/Options/Plugins/FixedHeader.php
@@ -7,6 +7,7 @@ namespace Yajra\DataTables\Html\Options\Plugins;
  *
  * @see https://datatables.net/extensions/fixedheader/
  * @see https://datatables.net/reference/option/fixedHeader
+ * @see https://datatables.net/reference/option/#fixedHeader
  */
 trait FixedHeader
 {
@@ -20,6 +21,62 @@ trait FixedHeader
     public function fixedHeader($value = true)
     {
         $this->attributes['fixedHeader'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set fixedHeader footer option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/fixedHeader.footer
+     */
+    public function fixedHeaderFooter(bool $value = true)
+    {
+        $this->attributes['fixedHeader']['footer'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set fixedHeader footerOffset option value.
+     *
+     * @param int $value
+     * @return $this
+     * @see https://datatables.net/reference/option/fixedHeader.footerOffset
+     */
+    public function fixedHeaderFooterOffset(int $value = 0)
+    {
+        $this->attributes['fixedHeader']['footerOffset'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set fixedHeader header option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/fixedHeader.header
+     */
+    public function fixedHeaderHeader(bool $value = true)
+    {
+        $this->attributes['fixedHeader']['header'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set fixedHeader headerOffset option value.
+     *
+     * @param int $value
+     * @return $this
+     * @see https://datatables.net/reference/option/fixedHeader.headerOffset
+     */
+    public function fixedHeaderHeaderOffset(int $value = 0)
+    {
+        $this->attributes['fixedHeader']['headerOffset'] = $value;
 
         return $this;
     }
