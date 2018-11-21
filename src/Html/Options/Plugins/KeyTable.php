2068b6f3a47e3adb21588fbda3de0502e3e46a4a
Arjay Q. Angeles <aqangeles@gmail.com>
2018-11-21 15:08:38 +0800

    Add full support for KeyTable plugin builder. As per https://datatables.net/reference/option/#scroller.

--- src/Html/Options/Plugins/KeyTable.php
+++ src/Html/Options/Plugins/KeyTable.php
@@ -7,6 +7,7 @@ namespace Yajra\DataTables\Html\Options\Plugins;
  *
  * @see https://datatables.net/extensions/keytable/
  * @see https://datatables.net/reference/option/keys
+ * @see https://datatables.net/reference/option/#keys
  */
 trait KeyTable
 {
@@ -20,6 +21,174 @@ trait KeyTable
     public function keys($value = true)
     {
         $this->attributes['keys'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys blurable option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.blurable
+     */
+    public function keysBlurable(bool $value = true)
+    {
+        $this->attributes['keys']['blurable'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys className option value.
+     *
+     * @param string $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.className
+     */
+    public function keysClassName($value = 'focus')
+    {
+        $this->attributes['keys']['className'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys clipboard option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.clipboard
+     */
+    public function keysClipboard(bool $value = true)
+    {
+        $this->attributes['keys']['clipboard'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys clipboardOrthogonal option value.
+     *
+     * @param string $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.clipboardOrthogonal
+     */
+    public function keysClipboardOrthogonal($value = 'display')
+    {
+        $this->attributes['keys']['clipboardOrthogonal'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys columns option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.columns
+     */
+    public function keysColumns($value)
+    {
+        $this->attributes['keys']['columns'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys editAutoSelect option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.editAutoSelect
+     */
+    public function keysEditAutoSelect(bool $value = true)
+    {
+        $this->attributes['keys']['editAutoSelect'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys editOnFocus option value.
+     *
+     * @param bool $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.editOnFocus
+     */
+    public function keysEditOnFocus(bool $value = true)
+    {
+        $this->attributes['keys']['editOnFocus'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys editor option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.editor
+     */
+    public function keysEditor($value)
+    {
+        $this->attributes['keys']['editor'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys editorKeys option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.editorKeys
+     */
+    public function keysEditorKeys($value = 'navigation-only')
+    {
+        $this->attributes['keys']['editorKeys'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys focus option value.
+     *
+     * @param string $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.focus
+     */
+    public function keysFocus($value)
+    {
+        $this->attributes['keys']['focus'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys keys option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.keys
+     */
+    public function keysKeys($value)
+    {
+        $this->attributes['keys']['keys'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set keys tabIndex option value.
+     *
+     * @param mixed $value
+     * @return $this
+     * @see https://datatables.net/reference/option/keys.tabIndex
+     */
+    public function keysTabIndex($value)
+    {
+        $this->attributes['keys']['tabIndex'] = $value;
 
         return $this;
     }
