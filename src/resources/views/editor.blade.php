661c0b9e0498b72664d78d26767c4e10a88cf823
Arjay Q. Angeles <aqangeles@gmail.com>
2019-06-26 16:05:45 +0800

    Use @json version to serialize editor options.

--- src/resources/views/editor.blade.php
+++ src/resources/views/editor.blade.php
@@ -2,27 +2,7 @@
     $.ajaxSetup({headers: {'X-CSRF-TOKEN': '{{csrf_token()}}'}});
     window.LaravelDataTables = window.LaravelDataTables || {};
     @foreach($editors as $editor)
-        var {{$editor->instance}} = window.LaravelDataTables["%1$s-{{$editor->instance}}"] = new $.fn.dataTable.Editor({
-        @if(is_array($editor->ajax))
-            ajax: @json($editor->ajax),
-        @else
-            ajax: "{{$editor->ajax}}",
-        @endif
-        table: "#{{$editor->table}}",
-        @if($editor->template)
-            template: "{{$editor->template}}",
-        @endif
-        @if($editor->display)
-            display: "{{$editor->display}}",
-        @endif
-        @if($editor->idSrc)
-            idSrc: "{{$editor->idSrc}}",
-        @endif
-        fields: @json($editor->fields),
-        @if($editor->language)
-            i18n: @json($editor->language)
-        @endif
-        });
+        var {{$editor->instance}} = window.LaravelDataTables["%1$s-{{$editor->instance}}"] = new $.fn.dataTable.Editor(@json($editor));
         {!! $editor->scripts  !!}
         @foreach ((array) $editor->events as $event)
             {{$editor->instance}}.on('{!! $event['event']  !!}', {!! $event['script'] !!});
