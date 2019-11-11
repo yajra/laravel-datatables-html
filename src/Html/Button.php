266febd55f5c0b9682795fb88865f3c856eeb8e5
om3rcitak <mail@omercitak.com>
2019-11-11 12:43:35 +0300

    hotfix | substring to substr

--- src/Html/Button.php
+++ src/Html/Button.php
@@ -185,7 +185,7 @@ class Button extends Fluent implements Arrayable
      */
     public function action($value)
     {
-        if (substring($value, 0, 8) == 'function') {
+        if (substr($value, 0, 8) == 'function') {
             $this->attributes['action'] = $value;
         } else {
             $this->attributes['action'] = "function(e, dt, node, config) { $value }";
