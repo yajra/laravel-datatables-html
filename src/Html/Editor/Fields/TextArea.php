1693bf9f07775b0076d17afe70a42d8eb4c0463e
Arjay Q. Angeles <aqangeles@gmail.com>
2020-04-02 15:42:38 +0800

    Add TextArea rows & cols fluent attribute setter.

--- src/Html/Editor/Fields/TextArea.php
+++ src/Html/Editor/Fields/TextArea.php
@@ -5,4 +5,22 @@ namespace Yajra\DataTables\Html\Editor\Fields;
 class TextArea extends Field
 {
     protected $type = 'textarea';
+
+    /**
+     * @param int $value
+     * @return static
+     */
+    public function rows($value)
+    {
+        return $this->attr('rows', $value);
+    }
+
+    /**
+     * @param int $value
+     * @return static
+     */
+    public function cols($value)
+    {
+        return $this->attr('cols', $value);
+    }
 }
