940e1fd70e3459e6977657072681cb4ae2d20539
redbastie <64050101+redbastie@users.noreply.github.com>
2020-09-23 03:02:05 -0400

    Wait for DOM before executing script.

--- src/resources/views/script.blade.php
+++ src/resources/views/script.blade.php
@@ -1,1 +1,3 @@
-(function(window,$){window.LaravelDataTables=window.LaravelDataTables||{};window.LaravelDataTables["%1$s"]=$("#%1$s").DataTable(%2$s);})(window,jQuery);
+window.addEventListener('DOMContentLoaded', function() {
+    (function(window,$){window.LaravelDataTables=window.LaravelDataTables||{};window.LaravelDataTables["%1$s"]=$("#%1$s").DataTable(%2$s);})(window,jQuery);
+});
