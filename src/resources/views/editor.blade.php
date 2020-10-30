11309631d56cddd545e466ea64a138eb2160a294
omer citak <mail@omercitak.com>
2020-10-30 05:11:16 +0300

    Make LaravelDataTables javascript namespace configurable. (#145)
    * made LaravelDataTables const to variable
    
    * add namespace variable to datatables-html config file
    
    * add default value to config functions

--- src/resources/views/editor.blade.php
+++ src/resources/views/editor.blade.php
@@ -1,12 +1,12 @@
 $(function(){
-    window.LaravelDataTables = window.LaravelDataTables || {};
+    window.{{ config('datatables-html.namespace', 'LaravelDataTables') }} = window.{{ config('datatables-html.namespace', 'LaravelDataTables') }} || {};
     $.ajaxSetup({headers: {'X-CSRF-TOKEN': '{{csrf_token()}}'}});
     @foreach($editors as $editor)
-        var {{$editor->instance}} = window.LaravelDataTables["%1$s-{{$editor->instance}}"] = new $.fn.dataTable.Editor({!! $editor->toJson() !!});
+        var {{$editor->instance}} = window.{{ config('datatables-html.namespace', 'LaravelDataTables') }}["%1$s-{{$editor->instance}}"] = new $.fn.dataTable.Editor({!! $editor->toJson() !!});
         {!! $editor->scripts  !!}
         @foreach ((array) $editor->events as $event)
             {{$editor->instance}}.on('{!! $event['event']  !!}', {!! $event['script'] !!});
         @endforeach
     @endforeach
-    window.LaravelDataTables["%1$s"] = $("#%1$s").DataTable(%2$s);
+    window.{{ config('datatables-html.namespace', 'LaravelDataTables') }}["%1$s"] = $("#%1$s").DataTable(%2$s);
 });
