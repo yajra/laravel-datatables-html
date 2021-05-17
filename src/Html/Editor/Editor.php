b891ddfcfe7ac29b0bf4fe3237415641ac6b8bbc
Arjay Angeles <aqangeles@gmail.com>
2021-05-17 12:55:30 +0800

    Implement authorizations on Editor builder.
    (cherry picked from commit dd0312c64912c013cd6f175c77d9c2caa9810a2c)

--- src/Html/Editor/Editor.php
+++ src/Html/Editor/Editor.php
@@ -5,12 +5,15 @@ namespace Yajra\DataTables\Html\Editor;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Support\Fluent;
+use Yajra\DataTables\Html\HasAuthorizations;
 use Yajra\DataTables\Utilities\Helper;
 use Yajra\DataTables\Html\Editor\Fields\Field;
 
 class Editor extends Fluent
 {
     use HasEvents;
+    use HasAuthorizations;
+
     const DISPLAY_LIGHTBOX   = 'lightbox';
     const DISPLAY_ENVELOPE   = 'envelope';
     const DISPLAY_BOOTSTRAP  = 'bootstrap';
@@ -20,7 +23,7 @@ class Editor extends Fluent
     /**
      * Editor constructor.
      *
-     * @param string $instance
+     * @param string|array $instance
      */
     public function __construct($instance = 'editor')
     {
@@ -37,6 +40,10 @@ class Editor extends Fluent
      */
     public static function make($instance = 'editor')
     {
+        if (is_array($instance)) {
+            $instance = $instance['editor'] ?? 'editor';
+        }
+
         return new static($instance);
     }
 
