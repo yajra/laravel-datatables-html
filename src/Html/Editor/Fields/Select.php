83961faf33e499aff904b69c022a7b38deb8e772
Arjay Angeles <aqangeles@gmail.com>
2021-09-12 20:38:47 +0800

    Add missing select field options as of Editor 1.5.4.

--- src/Html/Editor/Fields/Select.php
+++ src/Html/Editor/Fields/Select.php
@@ -2,7 +2,81 @@
 
 namespace Yajra\DataTables\Html\Editor\Fields;
 
+/**
+ * @see https://editor.datatables.net/reference/field/select
+ */
 class Select extends Field
 {
     protected $type = 'select';
+
+    /**
+     * Set field multiple value.
+     *
+     * @param  bool  $value
+     * @return $this
+     */
+    public function multiple(bool $value = true)
+    {
+        $this->attributes['multiple'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set field optionsPair value.
+     *
+     * @param  string|array  $label
+     * @param  string  $value
+     * @return $this
+     */
+    public function optionsPair($label = 'label', $value = 'value')
+    {
+        if (is_array($label)) {
+            $this->attributes['optionsPair'] = $label;
+        } else {
+            $this->attributes['optionsPair']['label'] = $label;
+            $this->attributes['optionsPair']['value'] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set field placeholder value.
+     *
+     * @param  string  $value
+     * @return $this
+     */
+    public function placeholder($value)
+    {
+        $this->attributes['placeholder'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set field placeholderDisabled value.
+     *
+     * @param  bool  $value
+     * @return $this
+     */
+    public function placeholderDisabled(bool $value)
+    {
+        $this->attributes['placeholderDisabled'] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set field placeholderValue value.
+     *
+     * @param  string  $value
+     * @return $this
+     */
+    public function placeholderValue($value)
+    {
+        $this->attributes['placeholderValue'] = $value;
+
+        return $this;
+    }
 }
