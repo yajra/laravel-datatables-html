19102149864c5df94c6d72d31dff0619fbb91fa0
Arjay Angeles <aqangeles@gmail.com>
2022-05-07 12:59:56 +0800

    Move callback config to core.

--- src/resources/config/config.php
+++ src/resources/config/config.php
@@ -16,12 +16,6 @@ return [
     ],
 
     /*
-     * Default condition to determine if a parameter is a callback or not.
-     * Callbacks needs to start by those terms or they will be casted to string.
-     */
-    'callback' => ['$', '$.', 'function'],
-
-    /*
      * Html builder script template.
      */
     'script' => 'datatables::script',
