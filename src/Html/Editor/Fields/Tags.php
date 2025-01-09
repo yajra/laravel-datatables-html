32f8a6e24d6c8fd5ed1bc60989ec6786b297a335
Arjay Angeles <aqangeles@gmail.com>
2025-01-09 15:38:09 +0800

    feat: add tags field support

---
+++ src/Html/Editor/Fields/Tags.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Yajra\DataTables\Html\Editor\Fields;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+/**
+ * @see https://editor.datatables.net/reference/field/tags
+ */
+class Tags extends Field
+{
+    protected string $type = 'tags';
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#ajax
+     */
+    public function ajax(bool|string|null $url = true): static
+    {
+        $this->attributes['ajax'] = $url;
+
+        return $this;
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#display
+     */
+    public function display(string $display): static
+    {
+        $this->attributes['display'] = $display;
+
+        return $this;
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#escapeLabelHtml
+     */
+    public function escapeLabelHtml(bool $escape): static
+    {
+        $this->attributes['escapeLabelHtml'] = $escape;
+
+        return $this;
+    }
+
+    /**
+     * @param  array {
+     *     addButton?: string
+     *     inputPlaceholder?: string
+     *     noResults?: string
+     *     title?: string
+     *     placeholder?: string
+     *  } $i18n
+     * @see https://editor.datatables.net/reference/field/tags#i18n
+     */
+    public function i18n(array $i18n): static
+    {
+        $this->attributes['i18n'] = array_merge($this->attributes['i18n'] ?? [], $i18n);
+
+        return $this;
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#i18n
+     */
+    public function addButton(string $text): static
+    {
+        return $this->i18n(['addButton' => $text]);
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#i18n
+     */
+    public function inputPlaceholder(string $text): static
+    {
+        return $this->i18n(['inputPlaceholder' => $text]);
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#i18n
+     */
+    public function noResults(string $text): static
+    {
+        return $this->i18n(['noResults' => $text]);
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#i18n
+     */
+    public function title(string $text): static
+    {
+        return $this->i18n(['title' => $text]);
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#i18n
+     */
+    public function placeholder(string $text): static
+    {
+        return $this->i18n(['placeholder' => $text]);
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#limit
+     */
+    public function limit(int $limit): static
+    {
+        $this->attributes['limit'] = $limit;
+
+        return $this;
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#multiple
+     */
+    public function multiple(bool $multiple = true): static
+    {
+        $this->attributes['multiple'] = $multiple;
+
+        return $this;
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#options
+     */
+    public function options(array|Arrayable $options): static
+    {
+        return parent::options($options);
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#separator
+     */
+    public function separator(string $separator = ','): static
+    {
+        return parent::separator($separator);
+    }
+
+    /**
+     * @see https://editor.datatables.net/reference/field/tags#unique
+     */
+    public function unique(bool $unique = true): static
+    {
+        $this->attributes['unique'] = $unique;
+
+        return $this;
+    }
+}
