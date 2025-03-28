e474918b5b6314762255ea0c08505783e9ef06a4
Abderrahim CHETIBI <chetibi.abderrahim@gmail.com>
2025-03-28 03:20:35 +0100

    fix: ensure DataTables initialization after the page fully loads

--- src/resources/views/script.blade.php
+++ src/resources/views/script.blade.php
@@ -1,4 +1,6 @@
+window.addEventListener("load", function() {
 $(function(){window.{{ config('datatables-html.namespace', 'LaravelDataTables') }}=window.{{ config('datatables-html.namespace', 'LaravelDataTables') }}||{};window.{{ config('datatables-html.namespace', 'LaravelDataTables') }}["%1$s"]=$("#%1$s").DataTable(%2$s);});
+});
 @foreach ($scripts as $script)
 @include($script)
 @endforeach
