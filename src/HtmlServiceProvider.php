810c5910447d8dbd19fb6b1295a02eb4f2fe471e
eduardo <eduardo@microvalencia.es>
2025-08-28 12:14:07 +0200

    changing laravelcollective/html form laravellux/html

--- src/HtmlServiceProvider.php
+++ src/HtmlServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Yajra\DataTables;
 
-use Collective\Html\HtmlServiceProvider as CollectiveHtml;
+use LaravelLux\Html\HtmlServiceProvider as CollectiveHtml;
 use Illuminate\Support\ServiceProvider;
 
 class HtmlServiceProvider extends ServiceProvider
